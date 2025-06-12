11e36ce93294e4fe512c730628401cb9c749ba43
Krishna <coderks36@gmail.com>
2025-06-12 23:21:10 +0530

    Layout updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-
-
 import "./globals.css";
+
 import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({ 
+const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-ibm-plex'
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
 });
 
 export const metadata: Metadata = {
@@ -17,19 +16,18 @@ export const metadata: Metadata = {
   description: "AI-powered image generator",
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-<body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{variables:{colorPrimary:'#624cf5'}}}>
+      <html lang="en">
+      <body className="text-5xl text-red-500 font-bold">
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
