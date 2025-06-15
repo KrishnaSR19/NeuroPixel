a378e50319853252bf29f9ad94491d7fc91583f2
Krishna <coderks36@gmail.com>
2025-06-15 15:46:01 +0530

    Sidebar styled

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { navLinks } from "@/constants";
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+import { Button } from "../ui/button";
+import { useUser } from "@clerk/nextjs";
 
 const Sidebar = () => {
   const pathname = usePathname();
+  const { user, isSignedIn } = useUser();
 
   return (
     <aside className="hidden h-screen w-72 flex-col bg-white p-5 shadow-md shadow-purple-200/50 lg:flex">
@@ -22,27 +25,84 @@ const Sidebar = () => {
           />
         </Link>
 
-        <nav className="flex h-full flex-col justify-between gap-4">
+        <nav className="h-full flex-col justify-between md:flex md:gap-4">
           <SignedIn>
-            <ul className="w-full flex flex-col items-start gap-2">
-              {navLinks.map((link) => {
+            <ul className=" hidden w-full flex-col items-start gap-2 md:flex">
+              {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname;
 
                 return (
                   <li
                     key={link.route}
-                    className={` ${
+                    className={`flex justify-center items-center font-semibold text-[16px] leading-[140%] w-full whitespace-nowrap rounded-full bg-cover  transition-all hover:bg-purple-100 hover:shadow-inner ${
                       isActive
-                        ? "bg-purple-gradient text-white"
+                        ? "bg-purple-gradient text-amber-900"
                         : "text-gray-700"
                     }`}
                   >
-                    {link.label}
+                    <Link
+                      className=" font-semibold text-[16px] leading-[140%] flex size-full gap-4 p-4"
+                      href={link.route}
+                    >
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
                   </li>
                 );
               })}
             </ul>
+
+            <ul className="hidden w-full flex-col items-start gap-2 md:flex">
+             
+                {navLinks.slice(6).map((link) => {
+                  const isActive = link.route === pathname;
+
+                  return (
+                    <li
+                      key={link.route}
+                      className={`flex justify-center items-center font-semibold text-[16px] leading-[140%] w-full whitespace-nowrap rounded-full bg-cover  transition-all hover:bg-purple-100 hover:shadow-inner ${
+                        isActive
+                          ? "bg-purple-gradient text-white"
+                          : "text-gray-700"
+                      }`}
+                    >
+                      <Link
+                        className=" font-semibold text-[16px] leading-[140%] flex size-full gap-4 p-4"
+                        href={link.route}
+                      >
+                        <Image
+                          src={link.icon}
+                          alt="logo"
+                          width={24}
+                          height={24}
+                          className={`${isActive && "brightness-200"}`}
+                        />
+                        {link.label}
+                      </Link>
+                    </li>
+                  );
+                })}
+           
+              <li className="flex justify-center items-center cursor-pointer gap-2 p-4">
+                <UserButton afterSwitchSessionUrl="/"  />    <p className="text-sm font-semibold">
+      {/* {user.username || user.fullName || user.primaryEmailAddress?.emailAddress} */}
+    </p>
+              </li>
+            </ul>
           </SignedIn>
+
+          <SignedOut>
+            <Button
+              asChild
+              className="py-4 px-6 flex justify-center items-center gap-3 rounded-full  font-semibold text-[16px] leading-[140%] focus-visible:ring-offset-0 focus-visible:ring-transparent !important bg-purple-gradient bg-cover"
+            ></Button>
+          </SignedOut>
         </nav>
       </div>
     </aside>
