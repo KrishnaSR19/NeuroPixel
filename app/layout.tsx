4e681132f32c1c26def795f1e4a7a6621541c49d
Krishna <coderks36@gmail.com>
2025-06-17 16:25:06 +0530

    variable changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,7 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
-import { cn } from "@/lib/utils";
 import "./globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
-
-const IBMPlex = IBM_Plex_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-ibm-plex",
-});
 
 export const metadata: Metadata = {
   title: "NeuroPixel",
@@ -22,9 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{variables:{colorPrimary:'#624cf5'}}}>
+    <ClerkProvider appearance={{ variables: { colorPrimary: "#624cf5" } }}>
       <html lang="en">
-      <body className="text-5xl text-red-500 font-bold">
+        <body className="text-5xl text-red-500 font-bold">
           {children}
         </body>
       </html>
