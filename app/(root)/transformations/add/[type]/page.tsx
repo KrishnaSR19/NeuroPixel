87a720dfb0facb7f9f127acb7028dae8c5d49376
Krishna <coderks36@gmail.com>
2025-06-27 15:47:52 +0530

    Transformation Page prompts and user added

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,29 +1,41 @@
 import Header from "@/components/shared/Header";
 import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 
 type TransformationType = keyof typeof transformationTypes;
 
 type SearchParamProps = {
   params: { type: TransformationType } | Promise<{ type: TransformationType }>;
 };
-
 const AddTransformationTypePage = async ({ params }: SearchParamProps) => {
   // Await params if it is a Promise
   const resolvedParams = await params;
   const { type } = resolvedParams;
+  const { userId } = await auth();
+
+  if(!userId) redirect("/sign-in");
+
 
   const transformation = transformationTypes[type];
+
+  const user = await getUserById(userId);
 
   if (!transformation) {
     return <div>Transformation type not found</div>;
   }
-
   return (
     <>
     <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-    <TransformationForm/>
+    <TransformationForm action="Add"
+    userId={user._id} 
+    type={transformation.type as TransformationTypeKey}
+    creditBalance={user.creditBalance}
+    />
     </>
   );
 };
