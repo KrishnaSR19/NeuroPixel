971451e39a55192d94bacacf98107cd967232881
Krishna <coderks36@gmail.com>
2025-07-05 16:40:37 +0530

    Cloud Upload  Widget setup

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,25 +1,39 @@
 import React from "react";
 import { toast } from "sonner";
 import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
 
-const MediaUploader = () => {
-  const onUploadSuccessHandler = (result: any) => {
-    // Handle the upload result here
-  toast('Image uploaded successfully', {
-    description: '1 credit has been deducted from your account',
-    duration: 5000,
-    className: 'success-toast', // optional custom styling
-  });
-  };
-
-const onUploadErrorHandler = () => {
-  toast('Something went wrong while uploading', {
-    description: 'Please try again',
-    duration: 5000,
-    className: 'error-toast', // optional custom styling
-  });
+type MediaUploaderProps = {
+  onValueChange: (value: string) => void; // Function to handle value change
+  setImage: React.Dispatch<React.SetStateAction<any>>; // Function to set the image state
+  image: any; // Current image state
+  publicId: string; // Public ID of the image
+  type: string; // Type of the media (e.g., "image", "video")
 };
 
+const MediaUploader = ({
+  onValueChange,
+  setImage,
+  image,
+  publicId,
+  type,
+}: MediaUploaderProps) => {
+  const onUploadSuccessHandler = (result: any) => {
+    // Handle the upload result here
+    toast("Image uploaded successfully", {
+      description: "1 credit has been deducted from your account",
+      duration: 5000,
+      className: "success-toast", // optional custom styling
+    });
+  };
+
+  const onUploadErrorHandler = () => {
+    toast("Something went wrong while uploading", {
+      description: "Please try again",
+      duration: 5000,
+      className: "error-toast", // optional custom styling
+    });
+  };
 
   return (
     <>
@@ -32,7 +46,32 @@ const onUploadErrorHandler = () => {
         onSuccess={onUploadSuccessHandler}
         onError={onUploadErrorHandler}
       >
+        {({ open }) => (
+          <div className="flex flex-col gap-4">
+            <h3 className="font-bold text-[30px] leading-[140%] text-dark-600">
+              Original
+            </h3>
 
+            {publicId ? (
+              <></>
+            ) : (
+              <div className="flex justify-center items-center h-72 cursor-pointer flex-col gap-5 rounded-[16px] border border-dashed bg-purple-100/20 shadow-inner"
+                {...()=>open()}>
+                  <div className="rounded-[16px] bg-white  p-5 shadow-sm shadow-purple-200/50">
+                  <Image
+                    src="/assets/icons/add.svg"
+                    alt="Add Image"
+                    width={24}
+                    height={24}
+                  />
+                  
+                  </div>
+                  <p className="font-medium text-[14px] leading-[120%]">Click here to upload the image</p>
+
+              </div>
+            )}
+          </div>
+        )}
       </CldUploadWidget>
     </>
   );
