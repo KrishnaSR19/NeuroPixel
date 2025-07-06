213b8fb06a3c4e31c41256754fb8a61e01b1e930
Krishna <coderks36@gmail.com>
2025-07-06 19:09:29 +0530

    function to show image implemented if image and tranformation config are present

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import { dataUrl, debounce, getImageSize } from "@/lib/utils";
+import { CldImage } from "next-cloudinary";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 const TransformedImage = ({
   image,
   type,
@@ -32,10 +35,40 @@ const TransformedImage = ({
       </div>
 
       {image?.publicId && transformationConfig ? (
-        <div className="relative"></div>
+        <div className="relative">
+          <CldImage
+            width={getImageSize(type, image, "width")}
+            height={getImageSize(type, image, "height")}
+            src={image?.publicId}
+            alt={image.title}
+            sizes={"(max-width: 767px) 100vw, 50vw"}
+            placeholder={dataUrl as PlaceholderValue}
+            className="transformed-image"
+            onLoad={() => {
+              setIsTransforming && setIsTransforming(false);
+            }}
+            onError={() => {
+              debounce(() => {
+                setIsTransforming && setIsTransforming(false);
+              }, 8000)();
+            }}
+            {...transformationConfig}
+          />
+
+          {isTransforming && (
+            <div className="flex justify-center items-center absolute left-[50%] top-[50%] size-full -translate-x-1/2 -translate-y-1/2 flex-col gap-2 rounded-[10px] border bg-dark-700/90">
+              <Image
+                src="/assets/icons/spinner.svg"
+                width={50}
+                height={50}
+                alt="Transforming"
+              />
+            </div>
+          )}
+        </div>
       ) : (
         <div className="flex justify-center items-center p-14-medium h-full min-h-72 flex-col gap-5 rounded-[16px] border border-dashed bg-purple-100/20 shadow-inner">
-    Transformed Image
+          Transformed Image
         </div>
       )}
     </div>
