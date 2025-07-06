9b7571edc52c1153b99fc3187d02ac862194af2f
Krishna <coderks36@gmail.com>
2025-07-06 21:42:37 +0530

    Image actions - update image action implemented

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -34,11 +34,23 @@ export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase();
 
+    const imageToUpdate = await Image.findById(image._id);
+
+    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
+      throw new Error("Unauthorized or image not found");
+    }
+
+    const updatedImage = await Image.findByIdAndUpdate(
+      imageToUpdate._id,
+      image,
+      { new: true }
+    )
+
     revalidatePath(path);
 
-    return JSON.parse(JSON.stringify(image));
+    return JSON.parse(JSON.stringify(updatedImage));
   } catch (error) {
-    handleError;
+    handleError(error)
   }
 }
 
