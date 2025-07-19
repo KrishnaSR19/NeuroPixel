0a411d111cb5a6b13de7870a9dd5eddd0fc6e971
Krishna <coderks36@gmail.com>
2025-07-19 16:32:26 +0530

    Home route styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,34 @@
-'use client'
+import { navLinks } from "@/constants";
+import Link from "next/link";
+import React from "react";
 
-
-export default function Home() {
+const Home = () => {
   return (
-    <div >
-      <h1 className="text-3xl font-bold">Welcome to NeuroPixel</h1>
+    <>
+      <section className="sm:flex justify-center items-center hidden h-72 flex-col gap-4 rounded-[20px] border bg-banner bg-cover bg-no-repeat p-10 shadow-inner">
+        <h1 className="text-[36px] font-semibold sm:text-[44px] leading-[120%] sm:leading-[56px] max-w-[500px] flex-wrap text-center text-white shadow-sm">
+          Unleash Your Creative Vision with Neuropixel
+        </h1>
+        <ul className="flex justify-center items-center">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex justify-center flex-col gap-2"
+            >
+              <li>
 
-    </div>
-  )
-}
+              </li>
+              <p>
+                {link.label}
+              </p>
+
+            </Link>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default Home;
