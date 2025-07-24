0b88b2737b9247e44d6034e4f10b0d14442475b2
Krishna <coderks36@gmail.com>
2025-07-24 18:33:27 +0530

    Inegration of get all images on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,11 +3,18 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image"
 import { Collection } from "@/components/shared/Collections";
+import { getAllImages } from "@/lib/actions/image.actions";
 
-const Home = ({searchParams}:SearchParamProps) => {
+const Home = async ({searchParams}:SearchParamProps) => {
 
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string ) || "";
+
+  const images = await getAllImages({
+    limit: 9,
+    page,
+    searchQuery
+  });
 
   return (
     <>
@@ -35,7 +42,12 @@ const Home = ({searchParams}:SearchParamProps) => {
         </ul>
       </section>
       <section className="sm:mt-12">
-        <Collection />
+        <Collection 
+        hasSearch={true}
+        images={images?.data}
+        totalPages={images?.totalPage}
+        page={page}
+        />
 
       </section>
     </>
