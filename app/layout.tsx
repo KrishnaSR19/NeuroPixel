71323f097f2e2b52213d2150e276f2393a323f3d
Krishna <coderks36@gmail.com>
2025-09-28 18:37:40 +0530

    layout updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,28 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { IBM_Plex_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
+import { cn } from "@/lib/utils"; // import your cn helper here
+
 export const metadata: Metadata = {
   title: "NeuroPixel",
   description: "AI-powered image generator",
 };
 
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex',
+});
+
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider appearance={{ variables: { colorPrimary: "#624cf5" } }}>
       <html lang="en">
-        <body className="text-5xl text-red-500 font-bold">
+        <body className={cn("font-ibm-plex antialiased", IBMPlex.variable)}>
           {children}
           <Toaster position="top-center" richColors />
         </body>
